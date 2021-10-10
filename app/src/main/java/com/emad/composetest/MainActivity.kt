41c8e6b822f5a26a47f3a313245c75ff0bca5e8d
M Odd <emad.mollaei@gmail.com>
2021-10-10 12:12:28 +0330

    added light and dark mode previews

--- app/src/main/java/com/emad/composetest/MainActivity.kt
+++ app/src/main/java/com/emad/composetest/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.emad.composetest
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
 import com.emad.composetest.model.Message
 import com.emad.composetest.ui.theme.ComposeTestTheme
 
@@ -21,11 +37,47 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
-fun writeFirst(msg: Message = Message("Emad", "This is a message!")){
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
+fun writeFirst(msg: Message = Message("Emad", "This is a message!")) {
+    ComposeTestTheme() {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(R.drawable.me_pic),
+                contentDescription = "Contact profile picture",
+                Modifier
+                    .size(55.dp)
+                    .padding(4.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Column(
+                Modifier.padding(horizontal = 8.dp)
+            ) {
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                    Text(
+                        text = msg.body,
+                        style = MaterialTheme.typography.body2,
+                        modifier = Modifier.padding(all = 4.dp),
+                        )
+                }
+            }
+        }
     }
 }
