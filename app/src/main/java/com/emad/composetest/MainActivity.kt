9a2bae9fed784ea47150545c0f415ba38c59c1c2
M Odd <emad.mollaei@gmail.com>
2021-10-10 12:27:58 +0330

    added clickable rows

--- app/src/main/java/com/emad/composetest/MainActivity.kt
+++ app/src/main/java/com/emad/composetest/MainActivity.kt
@@ -7,13 +7,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,7 +35,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            writeFirst()
+            Conversation(messages = SampleData.conversationSample)
         }
     }
 }
@@ -61,8 +64,12 @@ fun writeFirst(msg: Message = Message("Emad", "This is a message!")) {
 
             Spacer(modifier = Modifier.width(10.dp))
 
+            var isExpanded by remember { mutableStateOf(false) }
+
             Column(
-                Modifier.padding(horizontal = 8.dp)
+                Modifier
+                    .padding(horizontal = 8.dp)
+                    .clickable { isExpanded = !isExpanded }
             ) {
                 Text(
                     text = msg.author,
@@ -74,10 +81,20 @@ fun writeFirst(msg: Message = Message("Emad", "This is a message!")) {
                     Text(
                         text = msg.body,
                         style = MaterialTheme.typography.body2,
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                         modifier = Modifier.padding(all = 4.dp),
-                        )
+                    )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message: Message ->
+            writeFirst(message)
         }
     }
 }
